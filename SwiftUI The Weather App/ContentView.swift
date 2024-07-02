0a1b91391473106c5a5cd982243f6bcae6f0c340
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2024-07-02 15:41:49 +0200

    Initial Commit

---
+++ SwiftUI The Weather App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI The Weather App
+//
+//  Created by Macbook Air 2017 on 2. 7. 2024..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI The Weather App/SwiftUI_The_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_The_Weather_AppApp.swift
+//  SwiftUI The Weather App
+//
+//  Created by Macbook Air 2017 on 2. 7. 2024..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_The_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
